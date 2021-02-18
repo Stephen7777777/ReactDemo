d75d3ae43f439179638800a8c83393f8ecd16b14
a9569817 <9569817@gmail.com>
2021-02-19 12:47:59 +1300

    all

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import {useState} from'react'
+
+const App = () =>{
+  const [tasks , setTasks] = useState([
+    {id:1,
+      text:'Doctors Appointment',
+      day: 'Feb 5th at 2:00pm',
+      reminder:true,
+     },
+     {
+       id:2,
+       text:'Meeting at School',
+       day: 'Feb 6th at 1:30pm',
+       reminder:true,
+     },
+     {
+       id:3,
+       text:'Food Shooping',
+       day: 'Feb 5th at 2:30pm',
+       reminder:false
+     },
+  ])
+  return(
+    <div className='container'>
+      <Header />
+      <Tasks tasks = {tasks}/>
+    </div>
+  )
+}
+
+// class App extends React.Component{
+//   render(){
+//     return <h1>Hello From a Class</h1>
+//   }
+// }
+ 
+export default App
